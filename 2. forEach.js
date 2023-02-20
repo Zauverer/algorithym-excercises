49dcf8f7c2199f2eeddedaa2a7f30ba33bbef1b3
Cristian <56987681+Zauverer@users.noreply.github.com>
2023-02-20 11:43:39 -0300

    Update 2. forEach.js

--- 2. forEach.js
+++ 2. forEach.js
@@ -1,3 +1,36 @@
+// RECORRER ARREGLO CON FUNCION forEach
+
+// FUNCION ANONIMA
+
 languajes.forEach(function(valor, indice, lenguajes){
     console.log(languajes);
 })
+
+
+
+languajes.forEach(function(valor, indice, lenguajes){
+    console.log(`${valor} se encuentra en el indice ${indice}`);
+})
+
+// DECLARANDO UNA FUNCION
+
+function recorrer(valor, indice, lenguajes){
+    console.log(`${valor} se encuentra en el indice ${indice}`);
+}
+
+languajes.forEach(recorrer)
+
+// FUNCION LAMBDA
+
+languajes.forEach((valor, indice, lenguajes) => {
+    console.log(`${valor} se encuentra en el indice ${indice}`);
+})
+
+
+// FUNCION LAMBDA EN VARIABLE
+
+let recorrerLam = (valor, indice, languajes) => {
+    console.log(`${valor} se encuentra en el indice ${indice}`)
+}
+
+lenguajes.forEach(recorrerLam)
